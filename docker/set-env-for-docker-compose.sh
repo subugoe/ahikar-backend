ea47d5bb9208e718f0365d5555e858576d9cccfc
Mathias Göbel <goebel@sub.uni-goettingen.de>
2020-03-04 12:25:28 +0100

    set correct doker env

--- docker/set-env-for-docker-compose.sh
+++ docker/set-env-for-docker-compose.sh
@@ -4,12 +4,12 @@ echo CI_COMMIT_REF_NAME=${CI_COMMIT_REF_NAME}
 
 case ${CI_COMMIT_REF_NAME} in
 "master")
-  echo "PORT=8092" > .env
+  echo "PORT=8092" > docker/.env
   ;;
 "develop")
-  echo "PORT=8093" > .env
+  echo "PORT=8093" > docker/.env
   ;;
 *)
-  echo "PORT=8094" > .env
+  echo "PORT=8094" > docker/.env
   ;;
 esac
