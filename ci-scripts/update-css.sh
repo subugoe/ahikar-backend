bda1eb8647d6e7d21505803435c8003244be37f1
Michelle Weidling <weidling@sub.uni-goettingen.de>
2021-04-13 08:35:16 +0200

    ci: check path

--- ci-scripts/update-css.sh
+++ ci-scripts/update-css.sh
@@ -2,13 +2,14 @@
 
 MD5OLD=$(md5sum exist-app/data/ahikar.css | cut -d ' ' -f 1)
 MD5NEW=$(md5sum ahikar.css | cut -d ' ' -f 1)
-echo $MD5OLD
-echo $MD5NEW
+
 if [ "$MD5OLD" != "$MD5NEW" ]; then 
     echo "CSS needs to be updated"
+    WKDIR=$(pwd)
     mkdir -p /tmp/this && cd /tmp/this || exit
     git clone git@gitlab.gwdg.de:subugoe/ahiqar/backend.git
-    mv ../../ahikar.css exist-app/data/ahikar.css
+    ls "$WKDIR"
+    mv "$WKDIR"/ahikar.css exist-app/data/ahikar.css
     git add exist-app/data/ahikar.css && git commit -m "update CSS" && git push
 else
     echo "CSS does not require an update"
