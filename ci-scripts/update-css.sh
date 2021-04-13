c4396b398d3d59472adce7f4904d6e6e315d4abb
mrodzis <weidling@sub.uni-goettingen.de>
2021-04-13 07:12:43 +0000

    Bugfix/#154 css isnt updated

---
+++ ci-scripts/update-css.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MD5OLD=$(md5sum exist-app/data/ahikar.css | cut -d ' ' -f 1)
+MD5NEW=$(md5sum ahikar.css | cut -d ' ' -f 1)
+
+if [ "$MD5OLD" != "$MD5NEW" ]; then 
+    echo "CSS needs to be updated"
+    WKDIR=$(pwd)
+    mkdir -p /tmp/this && cd /tmp/this || exit
+    git clone git@gitlab.gwdg.de:subugoe/ahiqar/backend.git
+    cd backend || exit
+    mv "$WKDIR"/ahikar.css exist-app/data/ahikar.css
+    git add exist-app/data/ahikar.css && git commit -m "update CSS" && git push
+else
+    echo "CSS does not require an update"
+fi
